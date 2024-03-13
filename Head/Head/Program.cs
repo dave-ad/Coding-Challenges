e386a57fb0bdeda45bdbd41d819e4d6fe1e87d7c
dave-ad <davidaderibigbe47@gmail.com>
2024-03-13 02:52:16 +0100

    Build your own Head

---
+++ Head/Head/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10;
+        string filename = null;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-n" && i + 1 < args.Length)
+            {
+                if (int.TryParse(args[i + 1], out int num))
+                {
+                    n = num;
+                }
+            }
+            else
+            {
+                filename = args[i];
+            }
+        }
+
+
+        if (filename == null)
+        {
+            // Read from standard input
+            ReadFromStandardInput(n);
+        }
+        else
+        {
+            // Read from file
+            //string filename = args.Length > 0 ? args[0] : null;
+            ReadFromFile(filename, n);
+        }
+    }
+
+    static void ReadFromFile(string filename, int n)
+    {
+        //string path = @"C:\Users\David\source\repos\Coding-Challenges\Head\Head\" + filename;
+        try
+        {
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string line;
+                int count = 0;
+                while ((line = sr.ReadLine()) != null && count < n)
+                {
+                    Console.WriteLine(line);
+                    count++;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error reading file: " + e.Message);
+        }
+
+
+    }
+
+    static void ReadFromStandardInput(int n)
+    {
+        string line;
+        int count = 0;
+        while ((line = Console.ReadLine()) != null && count < n)
+        {          
+            Console.WriteLine(line);
+            count++;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Coding Challenges
+
+Weekly coding challenge aimed to help me level up my skills through deliberate practice.
+
+These are challenges that I am using as exercises to learn a new programming language or technology.
+
+Each challenge will have me writing a full application or tool. Most of which will be based on real world tools and utilities.
+
+The challenges are picked to be small enough to completed in my spare time over a week or two and yet large enough to be complete working projects.
+
+[Link](https://codingchallenges.fyi/)
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/dave-ad/Calculator)
+
